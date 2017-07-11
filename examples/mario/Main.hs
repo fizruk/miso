965cf082607d1942cd4f2d868fd0902c99bed906
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2017-07-12 00:27:15 +0300

    Load images properly in mario example
    - Use data-files to get images installed along executables (jsexe)
    - Use Paths_miso to properly locate installed data-files
    - Rename images to match Haskell data constructors

--- examples/mario/Main.hs
+++ examples/mario/Main.hs
@@ -9,7 +9,9 @@ import qualified Data.Map      as M
 import           Data.Monoid
 
 import           Miso
-import           Miso.String
+import           Miso.String (MisoString, pack)
+
+import           Paths_miso
 
 data Action
   = GetArrows !Arrows
@@ -22,15 +24,43 @@ foreign import javascript unsafe "$r = performance.now();"
 main :: IO ()
 main = do
     time <- now
-    let m = mario { time = time }
-    startApp App { model = m, ..}
+    imgs <- loadImages
+    let m = mario time
+    startApp App
+      { model = m
+      , view = display imgs
+      , update = updateMario
+      , events = defaultEvents
+      , subs   = [ arrowsSub GetArrows
+                 , windowSub WindowCoords
+                 ]
+      }
+
+-- | An image is determined just by its location.
+type Image = FilePath
+
+-- | All images preloaded.
+-- We only need images for Mario's motion.
+data Images = Images
+  { imagesMotion :: Motion -> FilePath
+  }
+
+instance Eq Images where _ == _ = True
+
+-- | Load a bunch of values and make an index function.
+loadAll :: (Eq a, Applicative f) => (a -> f Image) -> [a] -> f (a -> Image)
+loadAll load xs = indexEnum <$> sequenceA (map load xs)
   where
-    update = updateMario
-    view   = display
-    events = defaultEvents
-    subs   = [ arrowsSub GetArrows
-             , windowSub WindowCoords
-             ]
+    indexEnum ps x = case lookup x (zip xs ps) of
+      Nothing -> "//:0"   -- this basically means no src
+      Just p  -> p
+
+loadImages :: IO Images
+loadImages = Images <$> loadAll loadMotionImage allMotions
+
+loadMotionImage :: Motion -> IO Image
+loadMotionImage Motion{..} = getDataFileName
+  ("examples/mario/imgs/" <> show motionStyle <> "/" <> show motionDirection <> ".gif")
 
 data Model = Model
     { x :: !Double
@@ -42,21 +72,43 @@ data Model = Model
     , delta :: !Double
     , arrows :: !Arrows
     , window :: !(Int,Int)
-    } deriving (Show, Eq)
+    } deriving (Eq)
 
 data Direction
   = L
   | R
-  deriving (Show,Eq)
+  deriving (Show, Eq, Enum, Bounded)
 
-mario :: Model
-mario = Model
+allDirections :: [Direction]
+allDirections = [minBound..maxBound]
+
+data MotionStyle
+  = Jump
+  | Walk
+  | Stand
+  deriving (Show, Eq, Enum, Bounded)
+
+allMotionStyles :: [MotionStyle]
+allMotionStyles = [minBound..maxBound]
+
+data Motion = Motion
+  { motionDirection :: Direction
+  , motionStyle     :: MotionStyle
+  } deriving (Show, Eq, Bounded)
+
+allMotions :: [Motion]
+allMotions = Motion
+  <$> allDirections
+  <*> allMotionStyles
+
+mario :: Double -> Model
+mario t = Model
     { x = 0
     , y = 0
     , vx = 0
     , vy = 0
     , dir = R
-    , time = 0
+    , time = t
     , delta = 0
     , arrows = Arrows 0 0
     , window = (0,0)
@@ -107,24 +159,20 @@ walk Arrows{..} m@Model{..} =
                | otherwise -> dir
     }
 
-display :: Model -> View action
-display m@Model{..} = marioImage
+display :: Images -> Model -> View action
+display Images{..} m@Model{..} = marioImage
   where
     (h,w) = window
-    verb = if | y > 0 -> "jump"
-              | vx /= 0 -> "walk"
-              | otherwise -> "stand"
-    d = case dir of
-            L -> "left"
-            R -> "right"
-    src  = "imgs/"<> verb <> "/" <> d <> ".gif"
+    verb = if | y > 0 -> Jump
+              | vx /= 0 -> Walk
+              | otherwise -> Stand
     groundY = 62 - (fromIntegral (fst window) / 2)
     marioImage =
       div_ [ height_ $ pack (show h)
            , height_ $ pack (show w)
            ] [ img_ [ height_ "37"
                     , width_ "37"
-                    , src_ src
+                    , src_ (pack (imagesMotion (Motion dir verb)))
                     , style_ (marioStyle m groundY)
                     ] [] ]
 
